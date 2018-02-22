f9c959cb5063d1f59c065390b6170aa2d4a84887
billchungiii <billchungiii@gmail.com>
2018-02-22 13:29:25 +0800

    加入專案檔案。

---
+++ CountdownAnimation/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CountdownAnimation
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int seconds;
+            if (int.TryParse(inputText.Text, out seconds))
+            {
+                CreateCountDownAnimation(seconds);
+            }
+        }
+
+        private void CreateCountDownAnimation(int seconds)
+        {
+            ObjectAnimationUsingKeyFrames animation = new ObjectAnimationUsingKeyFrames();
+            for (Int32 i = 0; i < (seconds  + 1); i++)
+            {
+                DiscreteObjectKeyFrame clockKeyFrame = new DiscreteObjectKeyFrame();
+                clockKeyFrame.KeyTime = KeyTime.FromTimeSpan(new TimeSpan(0, 0, i));
+                clockKeyFrame.Value = ConvertSecondsToClock(seconds - i);
+                animation.KeyFrames.Add(clockKeyFrame);
+            }
+            animation.Duration = new Duration(new TimeSpan(0, 0, seconds));
+            Storyboard.SetTarget(animation, countDownText);
+            Storyboard.SetTargetProperty(animation, new PropertyPath(TextBlock.TextProperty));            
+            var storyboard = new Storyboard();           
+            storyboard.Children.Add(animation);           
+            storyboard.Begin();
+        }
+
+        private string ConvertSecondsToClock(int value)
+        {
+            TimeSpan s = new TimeSpan(0, 0, value);
+            return s.ToString(@"h\:mm\:ss");
+        }
+    }
+}
---
+++ CountdownApp/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+// 空白頁項目範本已記錄在 https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x404
+
+namespace CountdownApp
+{
+    /// <summary>
+    /// 可以在本身使用或巡覽至框架內的空白頁面。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int seconds;
+            if (int.TryParse(inputText.Text, out seconds))
+            {
+                CreateCountDownAnimation(seconds);
+            }
+        }
+
+        private void CreateCountDownAnimation(int seconds)
+        {
+            ObjectAnimationUsingKeyFrames animation = new ObjectAnimationUsingKeyFrames();
+            for (Int32 i = 0; i < (seconds + 1); i++)
+            {
+                DiscreteObjectKeyFrame clockKeyFrame = new DiscreteObjectKeyFrame();
+                clockKeyFrame.KeyTime = KeyTime.FromTimeSpan(new TimeSpan(0, 0, i));
+                clockKeyFrame.Value = ConvertSecondsToClock(seconds - i);
+                animation.KeyFrames.Add(clockKeyFrame);
+            }
+            animation.Duration = new Duration(new TimeSpan(0, 0, seconds));
+
+            Storyboard.SetTarget(animation, countDownText);
+            Storyboard.SetTargetProperty(animation, "TextBlock.Text");
+            var storyboard = new Storyboard();
+            storyboard.Children.Add(animation);
+            storyboard.Begin();
+        }
+
+        private string ConvertSecondsToClock(int value)
+        {
+            TimeSpan s = new TimeSpan(0, 0, value);
+            return s.ToString(@"h\:mm\:ss");
+        }
+    }
+}
